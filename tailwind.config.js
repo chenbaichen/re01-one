54ddb6f54b83696901e958c16a6e4692a562d860
chenbaichen <815914706@qq.com>
2025-04-03 15:59:15 +0800

    Add files via upload

---
+++ tailwind.config.js
@@ -0,0 +1,24 @@
+module.exports = {
+  content: ['./*.html'],
+  theme: {
+    extend: {
+      colors: {
+        primary: '#0062FF',
+        secondary: '#00B7FF'
+      },
+      borderRadius: {
+        'none': '0px',
+        'sm': '2px',
+        DEFAULT: '4px',
+        'md': '8px',
+        'lg': '12px',
+        'xl': '16px',
+        '2xl': '20px',
+        '3xl': '24px',
+        'full': '9999px',
+        'button': '4px'
+      }
+    }
+  },
+  plugins: []
+} 
